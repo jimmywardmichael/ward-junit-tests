aaf19ba76c5e4776638821ef480cc3843592df5c
James Ward <jimmywardmichael@gmail.com>
2023-02-19 22:30:02 -0600

    finished

--- src/test/java/CohortTest.java
+++ src/test/java/CohortTest.java
@@ -1,12 +1,42 @@
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.*;
+import static org.junit.Assert.*;
+
 import java.util.ArrayList;
 import java.util.List;
+
 import static org.junit.Assert.assertTrue;
 
-public class CohortTest{
-    public static void testAddStudent() {
-        List<Student> students = new ArrayList<>();
-        Student student = new Student(3, "Kaylee");
-        Cohort.addStudent(student);
-        assertTrue(students.contains("Kaylee"));
+public class CohortTest {
+
+    Cohort students;
+    Student joshua;
+
+    @Before
+    public void setUp(){
+        students = new Cohort();
+        joshua = new Student(12345, "joshua");
+        students.addStudent(joshua);
+        joshua.addGrade(100);
+        joshua.addGrade(90);
     }
+
+    @Test
+    public void testAddStudent(){
+        System.out.println(students.getStudents());
+        assertTrue(students.getStudents().contains(joshua));
+//        assertTrue();
+    }
+
+    @Test
+    public void testGetStudents(){
+        assertEquals(1, students.getStudents().size());
+    }
+
+    @Test
+    public void testCohortAverage(){
+        assertEquals(95, students.getCohortAverage(), 0.0);
+    }
+
 }
